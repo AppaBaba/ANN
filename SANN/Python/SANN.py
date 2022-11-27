3982837658a31dac9c719793b86b7931ed8f1201
darleg <darleg@outlook.com>
2022-11-27 10:20:35 -0800

    Update SANN.py

--- SANN/Python/SANN.py
+++ SANN/Python/SANN.py
@@ -31,15 +31,17 @@ sw0 = 2*random.random((3,4)) - 1
 sw1 = 2*random.random((4,1)) - 1
 
 for i in range(10000):
+    # start of the forward propagation
     il = dti
     hl = sig(dot(il,sw0))
     ol = sig(dot(hl,sw1))
-    olerr = dto - ol  
-    oldlt = olerr*slope(ol)
-    hlerr = oldlt.dot(transpose(sw1))
-    hldlt = hlerr * slope(hl)
-    sw1 += transpose(hl).dot(oldlt)
-    sw0 += transpose(il).dot(hldlt)
+    # backpragation follows
+    olerr = dto - ol                     # calculate the output layer error
+    oldlt = olerr*slope(ol)              # multiply the output error by the derivative of the sigmoid
+    hlerr = oldlt.dot(transpose(sw1))    # calculate the hidden layer error that contribute the output layer
+    hldlt = hlerr * slope(hl)            # multiply the hidden error by the derivative of the sigmoid
+    sw1 += transpose(hl).dot(oldlt)      # adjust the synaptic weghts connecting hidden and output layers
+    sw0 += transpose(il).dot(hldlt)      # adjust the synaptic weghts connecting inputo and hidden layers
 
 print('Output')
 print(ol)
