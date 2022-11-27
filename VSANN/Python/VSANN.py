e1286aa7966816b153427fa65f8b7ce2466ac49b
darleg <darleg@outlook.com>
2022-11-26 20:06:00 -0800

    Update VSANN.py

--- VSANN/Python/VSANN.py
+++ VSANN/Python/VSANN.py
@@ -1,4 +1,4 @@
-# Very Simple forward propagation neural network
+# Very Simple neural network with backpropagation
 # only two layers input and output
 
 # sm    Sigmoid function
@@ -25,11 +25,13 @@ random.seed(1)
 sw = 2*random.random((3,1)) - 1
 
 for i in range(10000):
+    # start of the forword propagation
     il = dti
     ol = sm(dot(il,sw))
-    err = dto - ol
-    dlt = err * slope(ol)
-    sw += dot(il.T,dlt)
+    # backpropagation follows
+    err = dto - ol           # calculate the error
+    dlt = err * slope(ol)    # multiply the error by the derivative of the sigmoid
+    sw += dot(il.T,dlt)      # adjust the synaptic weghts
 
 print ("Output")
 print (ol)
